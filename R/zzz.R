11642d724b6e3d065cbbbfe8e1d1bd32c4338ff1
Martin Binder <mlr.developer@mb706.com>
2024-06-11 12:30:02 +0000

    version 1.0.0

--- R/zzz.R
+++ R/zzz.R
@@ -6,6 +6,13 @@
 #' @importFrom methods is
 "_PACKAGE"
 
+
+
+# data.table-variables to announce:
+# .init_given, .trafo
+
+utils::globalVariables(c("J", "id", "original_id", "owner_ps_index", ".tags", "tag", ".trafo", "trafo", ".", "cargo", "default", "cls", "cond", "on"))
+
 .onLoad = function(libname, pkgname) { # nolint
   # nocov start
   backports::import(pkgname)
